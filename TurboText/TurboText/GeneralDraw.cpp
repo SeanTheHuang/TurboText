d7743f16390caaee433e07e91cb0e950d5af0529
Hugo Adams(MDS) <hugogfadams@gmail.com>
2017-05-09 11:39:50 +1200

    Filled out GeneralDraw class with some cool functions B)

--- TurboText/TurboText/GeneralDraw.cpp
+++ TurboText/TurboText/GeneralDraw.cpp
@@ -1,4 +1,62 @@
 #include "GeneralDraw.h"
+void GeneralDraw::SetDrawColour(ECOLOUR _colour)   
+{
+	switch (_colour)
+	{
+	case col_white_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		break;
+	}
+	case col_red_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED);
+		break;
+	}
+	case col_green_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+		break;
+	}
+	case col_yellow_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
+		break;
+	}
+	case col_blue_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_BLUE);
+		break;
+	}
+	case col_magenta_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE);
+		break;
+	}
+	case col_cyan_black:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		break;
+	}
+	case col_black_grey:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_INTENSITY);
+		break;
+	}
+	case col_black_white:
+	{	
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE);
+		break;
+	}
+
+	default:
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+		break;
+	}
+	}
+}
+
 
 void GeneralDraw::DrawMapOutline()
 {
@@ -6,6 +64,8 @@ void GeneralDraw::DrawMapOutline()
 	char b = 205;//horizontal lines
 
 	//choose colour here
+	SetDrawColour(col_white_black);
+
 	GoToXY(3, 1);
 	for (int i = 0; i < 74; i++)
 	{
@@ -46,10 +106,120 @@ void GeneralDraw::DrawMapOutline()
 	std::cout << a;
 }
 
-void GoToXY(int _iX, int _iY)
+void GeneralDraw::GoToXY(int _iX, int _iY)
 {
 	COORD point;
 	point.X = _iX;
 	point.Y = _iY;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
+}
+
+int GeneralDraw::GetXOffset()
+{
+	return 3;
+}
+
+int GeneralDraw::GetYOffset()
+{
+	return 2;
+}
+
+void GeneralDraw::ClearArena()      
+{
+	SetDrawColour(col_white_black);
+	
+	for (int i = 0; i < 44; i++)
+	{
+		GoToXY(GetXOffset(), GetYOffset() + i);
+		for (int j = 0; j < 74;j++)
+		{
+			std::cout << " ";
+		}
+	}
+}
+
+void GeneralDraw::ClearScreen()
+{
+	SetDrawColour(col_white_black);
+
+	for (int i = 0; i < 60; i++)
+	{
+		GoToXY(0, i);
+		for (int j = 0;j < 80;j++)
+		{
+			std::cout << " ";
+		}
+	}
+	GoToXY(0, 0);
+}
+
+void GeneralDraw::ClearRectangle(int _iX, int _iY, int _iWidth, int _iHeight)
+{
+	if ((_iX > 80) || (_iWidth > 80))
+	{
+		return;
+	}
+	GoToXY(_iX, _iY);
+
+	for (int i = 0; i < _iHeight+1; i++)
+	{
+		GoToXY(_iX, _iY + i);
+		for (int j = 0; j < _iWidth+1; j++)
+		{
+			std::cout << " ";
+		}
+	}
+}
+
+void GeneralDraw::DrawRectangle(int _iX, int _iY, int _iWidth, int _iHeight)
+{
+	if ((_iX > 80) || (_iWidth > 80))
+	{
+		return;
+	}
+	GoToXY(_iX, _iY);
+	char a = 205;
+
+	for (int i = 0; i < _iWidth; i++)
+	{
+		std::cout << a;
+	}
+
+	GoToXY(_iX, _iY + _iHeight);
+	for (int i = 0; i < _iWidth; i++)
+	{
+		std::cout << a;
+	}
+
+	a = 186;
+
+	GoToXY(_iX, _iY + 1);
+	for (int i = 0; i < _iHeight; i++)
+	{
+		GoToXY(_iX, _iY + 1 + i);
+		std::cout << a;
+	}
+
+	GoToXY(_iX + _iWidth, _iY + 1);
+	for (int i = 0; i < _iHeight; i++)
+	{
+		GoToXY(_iX + _iWidth, _iY + 1+i);
+		std::cout << a;
+	}
+
+	a = 201;
+	GoToXY(_iX, _iY);
+	std::cout << a;
+
+	a = 187;
+	GoToXY(_iX + _iWidth, _iY);
+	std::cout << a;
+
+	a = 200;
+	GoToXY(_iX, _iY + _iHeight);
+	std::cout << a;
+
+	a = 188;
+	GoToXY(_iX + _iWidth, _iY + _iHeight);
+	std::cout << a;
 }
--- TurboText/TurboText/GeneralDraw.h
+++ TurboText/TurboText/GeneralDraw.h
@@ -4,14 +4,38 @@
 #include <Windows.h>
 #include <iostream>
 
-void GoToXY(int _iX, int _iY);
 class GeneralDraw
 {
 public:
-	void DrawMapOutline();
+	static enum ECOLOUR
+	{
+		col_white_black = 0,
+		col_red_black = 1,
+		col_green_black = 2,
+		col_yellow_black = 3,
+		col_blue_black = 4,
+		col_magenta_black = 5,
+		col_cyan_black = 6,
+		col_black_grey = 7,
+		col_black_white = 8
+	};
+
+	static void GoToXY(int _iX, int _iY);//goes to character coordinate on console
+	static void DrawMapOutline();
+	static void SetDrawColour(ECOLOUR _colour);
+
+	static void ClearArena();//clears in the white box
+	static void ClearScreen();//clears entire screen to black
+
+	static void ClearRectangle(int _iX, int _iY, int _iWidth, int _iHeight); //clears a rectangular space
+	static void DrawRectangle(int _iX, int _iY, int _iWidth, int _iHeight); //draws a rectangle with corners
+
+	static int GetXOffset();
+	static int GetYOffset();
+
 private:
-
-
+	const int iXOffset = 2;
+	const int iYOffset = 3;
 };
 
 #endif
