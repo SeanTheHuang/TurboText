2c6214e19219a8c8e110bdb87922267964300e05
deen.sim6878 <deen.sim6878@1808C017>
2017-05-09 11:36:30 +1200

    Created PlayerTurbo Class

--- TurboText/TurboText/PlayerTurbo.cpp
+++ TurboText/TurboText/PlayerTurbo.cpp
@@ -1,2 +1,59 @@
 #include "PlayerTurbo.h"
 
+void PlayerTurbo::SetSpeed(int _i)
+{
+	iSpeed = _i;
+};
+int PlayerTurbo::GetSpeed()
+{
+	return iSpeed;
+};
+
+void PlayerTurbo::SetTrailWidth(int _i)
+{
+	iTrailWidth = _i;
+};
+int PlayerTurbo::GetTrailWidth()
+{
+	return iTrailWidth;
+};
+
+void PlayerTurbo::SetTailTouch(bool _b)
+{
+	bCanTouchOwnTail = _b;
+};
+bool PlayerTurbo::GetTailTouch()
+{
+	return bCanTouchOwnTail;
+};
+
+void PlayerTurbo::SetXCoord(int _i)
+{
+	iPlayerXCoord = _i;
+};
+
+void PlayerTurbo::SetYCoord(int _i)
+{
+	iPlayerYCoord = _i;
+};
+
+
+void PlayerTurbo::SetDirection(Direction _dDir)
+{
+	dDirection = _dDir;
+};
+
+void PlayerTurbo::SetPlayerColor(Color _cCol)
+{
+	cPlayerColor = _cCol;
+};
+
+void PlayerTurbo::SetTrailColor(Color _cCol)
+{
+	cTrailColor = _cCol;
+};
+
+void PlayerTurbo::SetPlayerUpgradeColor(Color _cCol)
+{
+	cPlayerWithUpgradeColor = _cCol;
+};
--- TurboText/TurboText/PlayerTurbo.h
+++ TurboText/TurboText/PlayerTurbo.h
@@ -1,8 +1,59 @@
 #ifndef  _PLAYER_TURBO
 #define  _PLAYER_TURBO
 
+enum Color
+{
+	RED = 0,
+	PINK = 1,
+	ORANGE = 2,
+	BLUE = 4,
+	TEAL = 5,
+	GREEN = 6
+};
+
+enum Direction
+{
+	UP = 0,
+	RIGHT = 1,
+	DOWN = 2,
+	LEFT = 3,
+};
+
 class PlayerTurbo
 {
+public:
+	void SetSpeed(int _i);
+	int GetSpeed();
+
+	void SetTrailWidth(int _i);
+	int GetTrailWidth();
+
+	void SetTailTouch(bool _b);
+	bool GetTailTouch();
+
+	void SetXCoord(int _i);
+
+	void SetYCoord(int _i);
+
+	void SetDirection(Direction _dDir);
+
+	void SetPlayerColor(Color _cCol);
+
+	void SetTrailColor(Color _cCol);
+
+	void SetPlayerUpgradeColor(Color _cCol);
+
+private:
+	int iSpeed = 1;
+	int iTrailWidth;
+	bool bCanTouchOwnTail;
+	int iPlayerXCoord;
+	int iPlayerYCoord;
+
+	Direction dDirection;
+	Color cPlayerColor;
+	Color cTrailColor;
+	Color cPlayerWithUpgradeColor;
 
 
 };
